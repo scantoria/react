9d6d8ad9f4003b7c948c1558eb2af9920e22ce86
Stephen C. Cantoria <scantoria@gmail.com>
2020-09-19 09:21:11 -0400

    Header example

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import Header from "./HeaderComponent";
 import Directory from "./DirectoryComponent";
 import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
@@ -15,17 +15,13 @@ class Main extends Component{
   }
 
   onCampsiteSelect(campsiteId) {
-        this.setState({selectedCampsite: campsiteId});
-    }
+    this.setState({selectedCampsite: campsiteId});
+  }
     
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">NuCamp</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header />
         <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)} />
         <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
       </div>
