63832a31802b7eb51e5fc8d48f7d021386cbaf26
Stephen C. Cantoria <scantoria@gmail.com>
2020-09-25 21:41:42 -0400

    redux

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, 
-    NavItem, Jumbotron, Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
+    NavItem, Jumbotron, Button, Modal, ModalHeader, ModalBody, 
+    Form, FormGroup, Input, Label } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component{
@@ -14,6 +15,7 @@ class Header extends Component{
 
         this.toggleNav = this.toggleNav.bind(this);
         this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
     }
 	toggleNav() {
         this.setState({
@@ -25,6 +27,12 @@ class Header extends Component{
         this.setState({
             isModalOpen: !this.state.isModalOpen
         });
+    }
+
+    handleLogin(event){
+        alert(`Username: ${this.username.value} Password: ${this.password.value} Remember: ${this.remember.checked}`);
+        this.toggleModal();
+        event.preventDefault();
     }
 
     render() {
@@ -79,7 +87,23 @@ class Header extends Component{
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                     <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
                     <ModalBody>
-
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username" innerRef={input => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password" innerRef={input => this.password = input} />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember" innerRef={input => this.remember = input} />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
                     </ModalBody>
                 </Modal>
             </React.Fragment>
---
+++ src/redux/reducer.js
@@ -0,0 +1,15 @@
+import { CAMPSITES } from '../shared/campsites';
+import { PARTNERS } from '../shared/partners';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+
+const initialState = {
+    campsites: CAMPSITES,
+    partners: PARTNERS,
+    comments: COMMENTS,
+    promotions: PROMOTIONS
+};
+
+export const Reducer = (state = initialState, action) => {
+    return state;
+};
