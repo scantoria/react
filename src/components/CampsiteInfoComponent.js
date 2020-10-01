de498c5b8930f0f088bd3e2d7c192a60db8f115f
Stephen C. Cantoria <scantoria@gmail.com>
2020-09-30 22:25:54 -0400

    redux-thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,147 @@
-import React from 'react';
-import {Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import React, { Component } from 'react';
+import {Card, CardImg, CardText, CardBody, Breadcrumb, 
+    BreadcrumbItem, Button, Modal, ModalBody, ModalHeader, 
+    Row, Col, Label} from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
+
+const required = val => val && val.length;
+const minLength = len => val => val && (val.length >= len);
+const maxLength = len => val => !val || (val.length <= len);
+
+class CommentForm extends Component{
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            isModalOpen: false
+        };
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    toggleModal(){
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSubmit(values){
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                <Button className="btn btn-light" outline onClick={this.toggleModal}>
+                    <i className="fa fa-pencil"/> Submit Comment
+                </Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader  toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={values => this.handleSubmit(values)}>
+                            <Row className="form-group">
+                                <Col>
+                                <Label htmlFor="rating">Rating</Label>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col>
+                                    <Control.select model=".select" 
+                                        id="rating" 
+                                        name="rating"
+                                        className="form-control">
+                                            <option value="5">5</option>
+                                            <option value="1">1</option>
+                                            <option value="2">2</option>
+                                            <option value="3">3</option>
+                                            <option value="4">4</option>
+                                            <option value="5">5</option>
+                                    </Control.select>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col>
+                                <Label >Name</Label>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col>
+                                    <Control.text model=".name" 
+                                        id="name" 
+                                        name="name"
+                                        placeholder="Name"
+                                        className="form-control"
+                                        validators={{
+                                            required,
+                                            minLength: minLength(2),
+                                            maxLength: maxLength(25)
+                                        }}
+                                    />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".name"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            required: "Required",
+                                            minLength: "Must be at least 2 characters",
+                                            maxLength: "No longer than 25 characters"
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col>
+                                <Label>Comment</Label>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col>
+                                    <Control.textarea model=".comment" 
+                                        id="comment" 
+                                        name="comment"
+                                        placeholder="Comments"
+                                        className="form-control">
+                                        validators={{
+                                            required,
+                                            minLength: minLength(15),
+                                            maxLength: maxLength(1000)
+                                        }}
+                                    </Control.textarea>
+                                    <Errors
+                                        className="text-danger"
+                                        model=".comment"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            required: "Required",
+                                            minLength: "Must be at least 15 characters",
+                                            maxLength: "No longer than 1000 characters"
+                                        }}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col className="col-3">
+                                    <Control.button model=".submit" 
+                                        id="submit" 
+                                        name="submit"
+                                        placeholder="Submit"
+                                        className="form-control btn btn-primary">Submit
+                                    </Control.button>
+                                </Col>
+                            </Row>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>   
+            </React.Fragment>
+        );
+    }
+}
 
 function RenderCampsite({campsite}){
     return(
@@ -15,7 +156,7 @@ function RenderCampsite({campsite}){
     );
 }
 
-function RenderComments({comments}){
+function RenderComments({comments, addComment, campsiteId}){
     if(comments){
         return(
             <div className="col-md-5 m-1">
@@ -26,6 +167,7 @@ function RenderComments({comments}){
                     <p className="font-italic">--{comments.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))}</p>
                 </div>
                 )}
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         );
     } 
@@ -36,6 +178,28 @@ function RenderComments({comments}){
 }
 
 function CampsiteInfo(props){
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
     if(props.campsite){
         return (
             <div className="container">
@@ -51,7 +215,12 @@ function CampsiteInfo(props){
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments 
+                        comments={props.comments} 
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
+                    
                 </div>
             </div>
         );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { addComment, fetchCampsites } from '../redux/ActionCreators';
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Directory from "./DirectoryComponent";
@@ -18,25 +19,38 @@ const mapStateToProps = state => {
   }
 }
 
+const mapDispatchToProps = {
+  addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
+    fetchCampsites: () => (fetchCampsites())
+};
+
 class Main extends Component{
+	componentDidMount() {
+    this.props.fetchCampsites();
+  }
 
   render() {
     const HomePage = () => {
       return (
-        <Home 
-          campsite={this.props.campsites.filter(campsite => campsite.featured)[0]}
-          promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
-          partner={this.props.partners.filter(partner => partner.featured)[0]}
+        <Home
+            campsite={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
+            campsitesLoading={this.props.campsites.isLoading}
+            campsitesErrMess={this.props.campsites.errMess}
+            promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
+            partner={this.props.partners.filter(partner => partner.featured)[0]}
         />
       );
     }
 
     const CampsiteWithId = ({match}) => {
       return (
-          <CampsiteInfo 
-              campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-              comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
-          />
+        <CampsiteInfo 
+            campsite={this.props.campsites.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+            isLoading={this.props.campsites.isLoading}
+            errMess={this.props.campsites.errMess}
+            comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+            addComment={this.props.addComment}
+        />
       );
     };
 
@@ -57,4 +71,4 @@ class Main extends Component{
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
